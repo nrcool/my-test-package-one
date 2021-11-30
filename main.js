8e19beaebfb9a55a44b3bbe334077a1dc7796ba9
nrcool <nomanalam7214@gmail.com>
2021-11-30 13:18:26 +0100

    inital commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// 2.       0 .      0 
+// Major . Minor . Patch
+
+//old version code is not fully compatible with new version
+//       add new features. error-bug / again /fix bugs
+
+//fix bug (patch release)
+function addition(a,b){
+    console.log(`toal of these two numbers are : ${a+b}`)
+}
+
+//add new feature (minor release)
+function multiply(a,b){
+    console.log(`product of these two numbers are : ${a*b}`)
+}
+
+module.exports= {addition , multiply}
+
